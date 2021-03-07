a79d4bb224b721269120e7f2a8b7c2d51c7899cb
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2021-03-07 13:19:51 +0000

    Updated BookingService :construction:
    Changed the way that bookings are read from text file to account for the updated model, created an updateTextFile method to clean up code :construction:

--- COMP1004SPA/Services/BookingService.cs
+++ COMP1004SPA/Services/BookingService.cs
@@ -14,12 +14,24 @@ namespace COMP1004SPA.Services{
             foreach(string s in file){
                 string[] split = s.Split(',');
                 
-                Booking b = new Booking(){Id = int.Parse(split[0]), name = split[1], phoneNum = split[2], partySize = int.Parse(split[3]), date = DateTime.Parse(split[4]) };
+                Booking b = new Booking(){Id = int.Parse(split[0]), name = split[1], phoneNum = split[2], partySize = int.Parse(split[3]), date = DateTime.Parse(split[4]),CovidPositive = bool.Parse(split[5]) };
                 bookings.Add(b);
                 
                 
             }
             return bookings;
+        }
+        public static void UpdateTextFile (string path = "./Bookings.txt"){
+                  File.WriteAllText("./Bookings.txt",string.Empty);
+            using( StreamWriter write = File.AppendText("./Bookings.txt")){
+
+            
+           
+            
+            foreach (Booking book in book){
+                 write.WriteLine(book.Id.ToString()+','+book.name+',' + book.phoneNum +',' + book.partySize.ToString()+','+ book.date);
+            }
+            }
         }
         public static int GetMax(){
             return book.Max(b => b.Id);
@@ -42,11 +54,12 @@ namespace COMP1004SPA.Services{
         }
         public Booking CreateBooking(Booking booking){
             booking.Id = Bnum++;
+            booking.CovidPositive = false;
             //StreamWriter write = new StreamWriter("./Bookings.txt");
             using( StreamWriter write = File.AppendText("./Bookings.txt")){
 
             
-            write.WriteLine(booking.Id.ToString()+','+booking.name+',' + booking.phoneNum +',' + booking.partySize.ToString()+','+ booking.date);
+            write.WriteLine(booking.Id.ToString()+','+booking.name+',' + booking.phoneNum +',' + booking.partySize.ToString()+','+ booking.date + booking.CovidPositive.ToString());
             }
             book.Add(booking);
             return booking;
@@ -64,19 +77,12 @@ namespace COMP1004SPA.Services{
         }
         public static void Delete(int id){
             book.RemoveAll(n => n.Id == id);
-           
-            File.WriteAllText("./Bookings.txt",string.Empty);
-            using( StreamWriter write = File.AppendText("./Bookings.txt")){
-
-            
-           
-            
-            foreach (Booking book in book){
-                 write.WriteLine(book.Id.ToString()+','+book.name+',' + book.phoneNum +',' + book.partySize.ToString()+','+ book.date);
-            }
-            }
+            UpdateTextFile();
+          
         }
-        
+        public static void MarkCovidPos(int id){
+
+        }
     }
 
 }
